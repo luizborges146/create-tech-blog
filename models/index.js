1222ee90e0b503140fa87d56b6671f0b180fbca9
luizborges146 <113717656+luizborges146@users.noreply.github.com>
2022-11-07 08:09:22 -0800

    Created the Model Index, adding the belongTo and Has many, creating associations for the user, blog and comment

---
+++ models/index.js
@@ -0,0 +1,42 @@
+const Blog = require('./Blog');
+const Comment = require('./Comments');
+const User = require('./User');
+
+
+// The user can have many blogs
+User.hasMany(Blog, {
+    foreignKey:'user_id',
+    onDelete:'CASCADE'
+});
+
+// A user can have many comments
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+// The blog can only belong to one user
+Blog.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+// A blog can have many comments
+Blog.hasMany(Comment, {
+    foreignKey: 'blog_id',
+    onDelete: 'CASCADE',
+});
+
+// The user's comments belong to the user only
+Comment.belongsTo(User, {
+    foreignKey:'user_id',
+    onDelete: 'CASCADE',
+});
+
+// The blog itself has comments that belong to it 
+Comment.belongsTo(Blog,{
+    foreignKey: 'blog_id',
+    onDelete: 'CASCADE',
+});
+
+module.exports = { User, Blog, Comment }
