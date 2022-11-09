6efcf3c1ab16fe18701a96c8e6bce8af20232ca3
luizborges146 <113717656+luizborges146@users.noreply.github.com>
2022-11-08 19:40:32 -0800

    fixed an issue realted to the sign in now it is working, the issue was realated to add an extra path in the file

--- public/js/signup.js
+++ public/js/signup.js
@@ -4,14 +4,11 @@ async function signupFormHandler(event) {
     const username = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
-    const twitter = document.querySelector('#twitter-signup').value.trim();
-    const github = document.querySelector('#github-signup').value.trim();
-
   
     if (username && email && password) {
       const response = await fetch('/api/users', {
         method: 'post',
-        body: JSON.stringify({ username, email, password, twitter, github }),
+        body: JSON.stringify({ username, email, password }),
         headers: { 'Content-Type': 'application/json' }
       });
   
